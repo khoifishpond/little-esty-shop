19f862a25ce2e7a81125c85e345a44d9dc7483a5
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-16 21:06:28 -0400

    Write tests for disabled and enabled merchants sections in admin merchants index page

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Admin Merchant Index page' do
   before(:each) do
     @merch1 = create(:merchant)
-    @merch2 = create(:merchant)
-    visit "/admin/merchants"
+    @merch2 = create(:merchant, status: 0)
+    visit admin_merchants_path
   end
 
   it 'shows the name of each merchant' do
@@ -21,5 +21,39 @@ RSpec.describe 'Admin Merchant Index page' do
     click_link("#{@merch2.name}")
 
     expect(current_path).to eq("/admin/merchants/#{@merch2.id}")
+  end
+
+  it 'has a button to enable/disable merchant' do
+    within("#disabled") do
+      within("#merchant-#{@merch1.id}") do
+        expect(page).to have_button("Enable")
+        click_button("Enable")
+        expect(current_path).to eq(admin_merchants_path)
+      end
+    end
+    @merch1.reload
+    expect(@merch1.status).to eq("enabled")
+  end
+
+  it 'has an enabled only section' do
+    within("#enabled") do
+      within("#merchant-#{@merch2.id}") do
+        expect(@merch2.status).to eq("enabled")
+        expect(page).to_not have_content("#{@merch1.name}")
+        expect(page).to have_content("#{@merch2.name}")
+        click_button "Disable"
+      end
+    end
+  end
+
+  it 'has an disabled only section' do
+    within("#disabled") do
+      within("#merchant-#{@merch1.id}") do
+        expect(@merch1.status).to eq("disabled")
+        expect(page).to have_content("#{@merch1.name}")
+          expect(page).to_not have_content("#{@merch2.name}")
+        click_on "Enable"
+      end
+    end
   end
 end
