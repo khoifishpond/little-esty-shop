5733aa89d41a58c35dcc0be3f64add89928f57a6
Andrew Massey <acmassey@ncsu.edu>
2021-09-16 22:07:14 -0400

    Add validations to the merchant_spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:items) }
   end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
   describe 'instance methods' do
     before(:each) do
       @merch1 = create(:merchant)
