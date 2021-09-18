bc2fdbed44d00f4e7f72a1f546d2f2bc9afb0045
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-17 17:24:57 -0700

    Create dashboard alias for merchants controlle

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchants do
+    get "/dashboard", controller: :merchants, action: :show, as: "dashboard"
     resources :items, controller: :merchant_items
     resources :invoices, controller: :merchant_invoices
   end
 
   namespace :admin do
+    get '/', to: 'dashboard#index'
     resources :merchants
     resources :invoices
-    get '/', to: 'dashboard#index'
   end
 
   resources :invoices, only: :update
-
   resources :invoice_items, only: :update
+  resources :welcome, only: :index
 
   get '/admin', to: 'admins#index'
-  get "/merchants/:merchant_id/dashboard", to: "merchants#dashboard"
 end
