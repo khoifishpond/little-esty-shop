8d106befa82e5d53b14f7f5ef1f6ab65ba1ecb3f
Henry Schmid <henry.schmid1@gmail.com>
2021-09-18 12:25:23 -0600

    Update form_with url to form_with model

--- app/controllers/invoice_items_controller.rb
+++ app/controllers/invoice_items_controller.rb
@@ -1,8 +1,9 @@
 class InvoiceItemsController < ApplicationController
   def update
-    merchant = Merchant.find(params[:merchant_id])
-    invoice = Invoice.find(params[:id])
-    invoice_item = InvoiceItem.find(params[:invoice_item_id])
+    # require 'pry'; binding.pry
+    merchant = Merchant.find(params[:invoice_item][:merchant_id])
+    invoice = Invoice.find(params[:invoice_item][:invoice_id])
+    invoice_item = InvoiceItem.find(params[:id])
     invoice_item.update(invoice_item_params)
 
     redirect_to "/merchants/#{merchant.id}/invoices/#{invoice.id}"
@@ -11,6 +12,6 @@ class InvoiceItemsController < ApplicationController
   private
 
   def invoice_item_params
-    params.permit(:status)
+    params.require(:invoice_item).permit(:status, :id)
   end
 end
