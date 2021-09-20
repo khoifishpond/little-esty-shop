48cca4d061e8dc33761b6c5afaed754f49780786
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-20 17:47:05 -0400

    Change application_controller to have new methods after refactor api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :github_api, :require_user
+  before_action :pull_request_api, :require_user, :contributor_api, :repo_api
 
   helper_method :current_user
 
@@ -14,7 +14,22 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def github_api
-    @github = Github.new
+  def repo_api
+    repo_data = GithubService.new.repo
+    @repo = Repo.new(repo_data)
+  end
+
+  def contributor_api
+    contributor_data = GithubService.new.contributors
+    users = ["timomitchel", "scottalexandra", "jamisonordway", "BrianZanti"]
+
+    @contributors = contributor_data.filter_map do |con|
+      Contributor.new(con) unless users.include?(con[:author][:login])
+    end
+  end
+
+  def pull_request_api
+    pr_data = GithubService.new.pull_requests
+    @pull_request = PullRequest.new(pr_data)
   end
 end
