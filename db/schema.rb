31731512928ded3121c952ea7468ace2347d2360
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-19 19:52:26 -0700

    Create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_004531) do
+ActiveRecord::Schema.define(version: 2021_09_20_001341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,14 @@ ActiveRecord::Schema.define(version: 2021_09_17_004531) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
   add_foreign_key "invoice_items", "invoices"
